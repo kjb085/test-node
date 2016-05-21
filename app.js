f9498cdaa3937866ff62aa8f27078d3ca520e658
Kenn Breece <kjb085@gmail.com>
2016-05-20 23:25:53 -0400

    Update root dir response

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ let express = require('express'),
     app = express();
 
 app.get('/', function(req, res){
-    res.send("<img src='http://stream1.gifsoup.com/view7/2903079/flipping-off-o.gif'/>");
+    // res.send("<img src='http://stream1.gifsoup.com/view7/2903079/flipping-off-o.gif'/>");
+    res.render('main');
 });
 
 app.listen(3000, function(){
