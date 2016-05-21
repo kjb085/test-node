4fa927dd87921d8f245d3945c074fb7da8a0eac5
Kenn Breece <kjb085@gmail.com>
2016-05-21 00:05:09 -0400

    Add in handlebars engine and expose static assets

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 'use strict';
 
-let express = require('express'),
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    hbs = exphbs.create(),
     app = express();
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(process.cwd() + "/assets"));
 
 app.get('/', function(req, res){
     // res.send("<img src='http://stream1.gifsoup.com/view7/2903079/flipping-off-o.gif'/>");
