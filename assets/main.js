fbc3cffd476df7000a1628f938ed42c798167f09
Kenn Breece <kjb085@gmail.com>
2016-05-21 00:05:27 -0400

    Add time to setTimeout

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     setTimeout(function(){
         $('#loading').css('display', 'none');
-        $('#flip-off').css('display', '');
-    });
+        $('#flip-off').css('display', 'block');
+    }, 1000);
 });
