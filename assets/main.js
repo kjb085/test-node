7be9ecf10ba04a5269ac83a856e36e2fa8b865d1
Kenn Breece <kjb085@gmail.com>
2016-05-21 23:56:41 -0400

    Attempt mobile work around for youtube autoplay

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
     setTimeout(function(){
         $('#loading').css('display', 'none');
+        $('#flip-off').append('<iframe id="iframe" width="420" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allowfullscreen></iframe>')
         $('#flip-off').css('display', 'block');
+        // document.getElementById('iframe').contentWindow.document.getElementById('buttonId').click()
+        // $('#flip-off').find('iframe').click(function(){
+        //     console.log('click');
+        // });
     }, 1000);
 });
